5e26475500d1be556e85004378ffc5b23e06b123
Jeremy Johns <jez.johns@gmail.com>
2023-05-16 12:21:56 +0000

    added delete and post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [cats, setCats] = useState([]);
+	const [form, setForm] = useState({
+		name: "",
+		colour: "",
+		hasClaws: true,
+		location: "",
+	});
+
+	useEffect(() => {
+		console.log("Used effect");
+		getCats();
+	}, []);
+
+	async function getCats() {
+		const API = "http://localhost:8080/cats";
+		const res = await axios.get(API);
+		setCats(res.data);
+	}
+
+	async function deleteCats(id) {
+		console.log(id);
+		const API = `http://localhost:8080/cats/${id}`;
+		await axios.delete(API);
+		//re-run getCats to re-render page
+		getCats();
+	}
+
+	async function postCat(event) {
+		// this is the event handler for submit
+		event.preventDefault(); // but want to reset the form later ...
+		const API = "http://localhost:8080/cats";
+		// post will pass form as body because it exactly matches the model
+		// as shown in app.post on !!!server!!!
+		// so body carries the new Cat
+		await axios.post(API, form);
+		getCats();
+		setForm({
+			name: "",
+			colour: "",
+			hasClaws: true,
+			location: "",
+		});
+	}
+
+	function handleChange(event) {
+		//     setForm({...form, name: event.target.value}) for name
+		setForm({ ...form, [event.target.name]: event.target.value });
+	}
+
+	return (
+		<div className="App">
+			<h1>Cats</h1>
+			<form onSubmit={postCat}>
+				<input
+					name="name"
+					placeholder="Name"
+					onChange={handleChange}
+					value={form.name}></input>
+				<input
+					name="colour"
+					placeholder="colour"
+					onChange={handleChange}
+					value={form.colour}></input>
+				<input
+					name="location"
+					placeholder="location"
+					onChange={handleChange}
+					value={form.location}></input>
+				<input type="submit" />
+				{/* <input name="hasClaws" placeholder="claws"></input> */}
+			</form>
+			<div className="catWrap">
+				{cats.map((cat) => {
+					return (
+						<div>
+							<h3>{cat.name}</h3>
+							<p>{cat.colour}</p>
+							<p>Claws? {cat.hasClaws}</p>
+							<p>{cat.location}</p>
+
+							<button onClick={() => deleteCats(cat._id)}>Delete</button>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
